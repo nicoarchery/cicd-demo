afcdb40f581906fbe38867a80e11f9e472a8843b
nicoarchery <nicojimev1234@gmail.com>
2026-04-29 10:17:38 -0500

    pom modificacion antigravity2

--- src/test/java/au/com/equifax/cicddemo/UserControllerIntTest.java
+++ src/test/java/au/com/equifax/cicddemo/UserControllerIntTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
