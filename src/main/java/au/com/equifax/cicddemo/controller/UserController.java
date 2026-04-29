2c7ec0a4f7064ab41186a361d210bebe7a21b8c5
nicoarchery <nicojimev1234@gmail.com>
2026-04-29 10:48:13 -0500

    pom modificacion antigravity5

--- src/main/java/au/com/equifax/cicddemo/controller/UserController.java
+++ src/main/java/au/com/equifax/cicddemo/controller/UserController.java
@@ -24,6 +24,11 @@ public class UserController {
         this.service=param;
     }
 
+    @GetMapping("/")
+    public String welcome() {
+        return "¡Bienvenido al Taller de CI/CD! El despliegue automático funciona correctamente.";
+    }
+
     @GetMapping("/users")
     public Collection<User> retrieveAllUsers() {
         return service.getUsers();
